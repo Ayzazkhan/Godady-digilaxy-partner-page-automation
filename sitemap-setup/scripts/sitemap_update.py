2e3022b46b87cf26ec512ca520cb38dbb0a5728b
Ayzazkhan <79360143+Ayzazkhan@users.noreply.github.com>
2026-01-08 02:45:13 +0500

    Update sitemap_update.py

--- sitemap-setup/scripts/sitemap_update.py
+++ sitemap-setup/scripts/sitemap_update.py
@@ -62,10 +62,10 @@ def update_sitemap(content, domain):
 
 def main():
     domain = os.environ.get("CURRENT_DOMAIN")
-    user = os.environ.get("FTP_USER", "all@logodesigners.us")
+    user = os.environ.get("FTP_USER", "all@all@nursingassignmenthelps.co.uk")
     password = os.environ.get("FTP_PASS", "A4tech@1234")
     
-    if user == "${FTP_USER}": user = "all@logodesigners.us"
+    if user == "${FTP_USER}": user = "all@all@nursingassignmenthelps.co.uk"
     if password == "${FTP_PASS}": password = "A4tech@1234"
     
     if not domain:
